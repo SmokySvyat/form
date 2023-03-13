fd8c611332704ccb49bc9647fa796b265f36e01f
Svyat <swcnova@gmail.com>
2023-03-13 22:28:56 +0700

    begining validation

---
+++ scripts/constants.js
@@ -0,0 +1,12 @@
+const validationOptions = {
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit',
+    inactiveButtonClass: 'form__submit_inactive',
+    inputErrorClass: 'form__input_invalid',
+    errorClass: 'form__input-error_active',
+    errorText: '.form__input-error',
+    errorClosestParent: '.form__section',
+    password: '#password',
+    passwordRepeat: '#password-repeat'
+  };
---
+++ scripts/validate.js
@@ -0,0 +1,101 @@
+function showError (errorElement, inputElement, options) {
+    errorElement.textContent = inputElement.validationMessage;
+    errorElement.classList.add(options.errorClass);
+    inputElement.classList.add(options.inputErrorClass);
+  };
+  
+function hideError (errorElement, inputElement, options) {
+    errorElement.textContent = '';
+    errorElement.classList.remove(options.errorClass);
+    inputElement.classList.remove(options.inputErrorClass);
+};
+
+function setButtonActive (submitElement, inactiveButtonClass) {
+    submitElement.removeAttribute('disabled');
+    submitElement.classList.remove(inactiveButtonClass);
+};
+  
+function setButtonInactive (submitElement, inactiveButtonClass) {
+    submitElement.setAttribute('disabled', 'true');
+    submitElement.classList.add(inactiveButtonClass);
+};
+
+function comparisonPassword (password, passwordRepeat) {
+    const passworD = document.querySelector(password).value;
+    const passwordRepeaT = document.querySelector(passwordRepeat).value
+    console.log(passworD)
+    if (passworD === passwordRepeaT) {
+        console.log('ok');
+    } else {
+        console.log('wrong')
+    }
+}
+
+function setInputState (inputElement, isValid, options) {
+    const inputSectionElement = inputElement.closest(options.errorClosestParent)
+    const errorElement = inputSectionElement.querySelector(options.errorText)
+    if (isValid) {
+      hideError (errorElement, inputElement, options)
+      comparisonPassword(options.password, options.passwordRepeat)
+    } else {
+      showError (errorElement, inputElement, options);
+    };
+};
+
+const toggleInputState = (inputElement, options) => {
+    const isValid = inputElement.validity.valid;
+    setInputState(inputElement, isValid, options)
+};
+
+const setEventListeners = (form, options) => {
+    const submitElement = form.querySelector(options.submitButtonSelector);
+    const inputs = Array.from(form.querySelectorAll(options.inputSelector));
+  
+    inputs.forEach(inputElement => {
+      inputElement.addEventListener('change', () => {
+        toggleInputState(inputElement, options);
+        toggleBtnState(inputs, submitElement, options.inactiveButtonClass);
+      });
+  })
+  const toggleBtnState = (inputs, submitElement, inactiveButtonClass) => {
+    const isFormValid = inputs.every((inputElement) => {
+      return inputElement.validity.valid;
+    });
+
+    if (isFormValid) {
+      setButtonActive (submitElement, inactiveButtonClass);
+    } else {
+      setButtonInactive (submitElement, inactiveButtonClass);
+    }
+  }
+
+  toggleBtnState(inputs, submitElement)
+};
+
+const enableValidation = ({
+    formSelector,
+    inputSelector,
+    submitButtonSelector,
+    inactiveButtonClass,
+    inputErrorClass,
+    errorClass,
+    errorText,
+    errorClosestParent,
+    password,
+    passwordRepeat
+  }) => {
+    const forms = Array.from(document.querySelectorAll(formSelector));
+    forms.forEach(form => {
+      setEventListeners(form, {
+        inputSelector,
+        submitButtonSelector,
+        inactiveButtonClass,
+        inputErrorClass,
+        errorClass,
+        errorText,
+        errorClosestParent,
+        password,
+        passwordRepeat
+      });
+    });
+  };
